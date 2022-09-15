ac4cb6bcc356f4db44a2eda018719f25abbbef7b
Ifazo <ziaulkarimifaz@gmail.com>
2022-09-15 12:23:05 +0600

    mistake catch

--- history.js
+++ history.js
@@ -34,7 +34,7 @@ function displayHistory() {
   <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
-
+// console.log(test);
     histories.appendChild(newRow);
   });
 }
